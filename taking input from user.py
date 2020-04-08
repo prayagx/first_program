a6b8411f79f295bb8c6d6a28f72b3cfa02ea25be
Prayag Gupta <guptaprayag16@gmail.com>
2020-04-08 22:49:53 +0530

    Add files via upload
    simple program

---
+++ taking input from user.py
@@ -0,0 +1,42 @@
+name=input("enter your name: ")
+age=input("enter your age: ")
+print("hello "+name+" your age is "+age)
+print("a simple calculator")
+def addition (num2,num3):
+    num2=input("enter a number: ")
+    num3=input("enter the 2nd number: ")
+    result=int(num2)+int(num3)
+    return result
+def subtraction (num2,num3):
+    num2 = input("enter a number: ")
+    num3 = input("enter the 2nd number: ")
+    result = int(num2) - int(num3)
+    return result
+def multiply (num2,num3):
+    num2 = input("enter a number: ")
+    num3 = input("enter the 2nd number: ")
+    result = int(num2)*int(num3)
+    return result
+def divide (num2,num3):
+    num2 = input("enter a number: ")
+    num3 = input("enter the 2nd number: ")
+    result = float(num2)/float(num3)
+    return result
+number=input("enter a character between(1-4):  ")
+print("select an operation.")
+print("1.add")
+print("2.subtract")
+print("3.multiply")
+print("4.divide")
+if number=='1':
+    print(addition(num2,num3))
+elif number=='2':
+    print(subtraction(num2,num3))
+elif number=='3':
+    print(multiply(num2,num3))
+elif number=='4':
+    print(divide(num2,num3))
+else:
+    print("wrong input")
+
+
